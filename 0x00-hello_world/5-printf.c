02397e0db5bc37bc22ddafe2ef40822b7e120492
jaygad <yjerrygad@gmail.com>
2022-12-14 15:15:03 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * use the function printf
  * compile with -Wall gcc without warning
+ * Return: Always 0
  */
-
 int main(void)
-
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
