f412360d651806d9f27bbce17bf29f7147ced773
jaygad <yjerrygad@gmail.com>
2022-12-22 13:05:51 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * in reverse order
+ * followed by a new line
+ * Return: 0 if success
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar (ch);
+	}
+	putchar('\n');
+	return (0);
+}
